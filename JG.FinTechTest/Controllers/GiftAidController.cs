5c5dde41cf4d61ff72b4f93e05e27464054aaff9
David Monaghan <davidmonaghan@tfl.gov.uk>
2020-02-08 17:37:36 +0000

    added swagger documentation

--- JG.FinTechTest/Controllers/GiftAidController.cs
+++ JG.FinTechTest/Controllers/GiftAidController.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JG.FinTechTest.Controllers
@@ -16,7 +16,17 @@ namespace JG.FinTechTest.Controllers
             _giftAidCalculator = giftAidCalculator;
         }
 
+        /// <summary>
+        /// Calculates GiftAid for a donation amount
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns>A gift aid response object containing the donation and gift aid amount</returns>
+        /// <response code="200">Returns the git aid response object</response>
+        /// <response code="400">If the amount is not a value between 2 and 100,000</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
         public ActionResult<GiftAidResponse> GiftAid(decimal amount)
         {
             if (amount < MinimumValue || amount > MaximumValue)
--- JG.FinTechTest/Startup.cs
+++ JG.FinTechTest/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace JG.FinTechTest
 {
@@ -27,11 +30,19 @@ namespace JG.FinTechTest
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSingleton<IGiftAidCalculator>(c => new GiftAidCalculator(20));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gift Aid Service", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,6 +54,13 @@ namespace JG.FinTechTest
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gift Aid Service");
+                c.RoutePrefix = string.Empty;
+            });
         }
     }
 }
